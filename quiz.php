7bd269007d90eb696de95cbcfa8b1b94d9ac4556
ZuLu29 <markneilconde850@gmail.com>
2024-11-29 22:53:06 +0800

    adding question

--- quiz.php
+++ quiz.php
@@ -1,5 +1,24 @@
 <?php 
 include "connection.php";
+$questions = [
+    [
+        "question" => "What does PHP stand for?",
+        "options" => ["Personal Home Page", "Private Home Page", "PHP: Hypertext Preprocessor", "Public Hypertext Preprocessor"],
+        "answer" => 2
+    ],
+    [
+        "question" => "Which symbol is used to access a property of an object in PHP?",
+        "options" => [".", "->", "::", "#"],
+        "answer" => 1
+    ],
+    [
+        "question" => "Which function is used to include a file in PHP?",
+        "options" => ["include()", "require()", "import()", "load()"],
+        "answer" => 0
+    ]
+];
+
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
