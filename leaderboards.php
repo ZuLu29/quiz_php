1f0007162b6a196e5d0d79c8f52dafede2e777be
ZuLu29 <markneilconde850@gmail.com>
2024-11-30 12:07:33 +0800

    securing the result

--- leaderboards.php
+++ leaderboards.php
@@ -4,4 +4,10 @@ include = "connection.php";
 
 $query = "SELECT username, score, date_taken FROM leaderboard order by score desc, date_taken asc limit 10";
 $result = mysqli_query($conn, $query);
+
+if (!$result){
+    die("unable to connect to leaderboard: " . mysqli_error($conn));
+}
+
+$leaderboard = mysqli_fetch_all($result, MYSQLI_ASSOC);
 ?>
