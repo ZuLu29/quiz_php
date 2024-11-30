2afc1af7999d3552fa4200bcc6868d319c8d7d67
ZuLu29 <markneilconde850@gmail.com>
2024-11-30 12:03:18 +0800

    retrieving data from db

--- leaderboards.php
+++ leaderboards.php
@@ -1,4 +1,7 @@
 <?php
 include = "connection.php";
 
+
+$query = "SELECT username, score, date_taken FROM leaderboard order by score desc, date_taken asc limit 10";
+$result = mysqli_query($conn, $query);
 ?>
